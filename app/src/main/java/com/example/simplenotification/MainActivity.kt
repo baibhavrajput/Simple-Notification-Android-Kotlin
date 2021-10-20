43206e5df6b5d8de414b6958068b4f69cdac58a3
baibhavrajput <baibhavrajputt@gmail.com>
2021-10-20 22:04:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/simplenotification/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.simplenotification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            nm.createNotificationChannel(NotificationChannel("first","default", NotificationManager.IMPORTANCE_DEFAULT))
+
+        }
+        button.setOnClickListener{
+            val simpleNotification = NotificationCompat.Builder(this,"first")
+                .setContentTitle("Notification Title")
+                .setContentText("This is sample description of the notification")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+            nm.notify(1,simpleNotification)
+        }
+    }
+}
